45736672135f4c1c145dc04501c3243de272c574
socialintents <socialintents@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-15 15:19:30 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/email-list-builder-by-social-intents/trunk@893673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ email-list-build-by-socialintents.php
@@ -0,0 +1,425 @@
+<?php
+/*
+Plugin Name: Email List Builder by Social Intents
+Plugin URI: http://www.socialintents.com
+Description: Add a customizable and targeted email subscription widget to any page.  Integrates with MailChimp and Constant Contact as well as CSV Exports (more coming soon).  Additional widgets available! Free for 30 new email list subscribers a month.
+Version: 1.0.0
+Author: Social Intents
+Author URI: http://www.socialintents.com/
+*/
+
+$elb_domain = plugins_url();
+add_action('init', 'elb_init');
+add_action('admin_notices', 'elb_notice');
+add_filter('plugin_action_links', 'elb_plugin_actions', 10, 2);
+add_action('wp_footer', 'elb_insert',4);
+
+function elb_init() {
+    if(function_exists('current_user_can') && current_user_can('manage_options')) {
+        add_action('admin_menu', 'elb_add_settings_page');
+    }
+}
+
+function elb_insert() {
+
+    global $current_user;
+    if(strlen(get_option('elb_widgetID')) == 32 && get_option('elb_tab_text')) {
+        get_currentuserinfo();
+	echo("\n\n<!-- Social Intents Customization -->\n");
+        echo("<div id=\"social-intents-tab\" class=\"social-intents-tab\" style=\"visibility:hidden;\"></div>\n");
+        echo("<div id=\"socialintents-offer\"></div>\n");
+        echo("<script type=\"text/javascript\">\n");
+        echo("var socialintents_vars ={\n");
+        echo("'widgetId':'".get_option('elb_widgetID')."',\n");
+        echo("'tabLocation':'".get_option('elb_tab_placement')."',\n");
+        echo("'tabText':'".get_option('elb_tab_text')."',\n");
+        echo("'type':'email',\n");
+        echo("'tabColor':'".get_option('elb_tab_color')."',\n");
+        echo("'popupHeight':'130px', \n");
+        echo("'tabWidth':'280px',\n");
+        echo("'marginRight':'60px', \n");
+        echo("'headerTitle':'".get_option('elb_header_text')."'\n");
+        echo("};\n");
+        echo("(function() {function socialintents(){\n");
+        echo("    var siJsHost = ((\"https:\" === document.location.protocol) ? \"https://\" : \"http://\");\n");
+        echo("    var s = document.createElement('script');s.type = 'text/javascript';s.async = true;s.src = siJsHost+'www.socialintents.com/api/socialintents.js';\n");
+        echo("    var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};\n");
+        echo("if (window.attachEvent)window.attachEvent('onload', socialintents);else window.addEventListener('load', socialintents, false);})();\n");
+        echo("</script>\n");
+    }
+}
+
+function elb_notice() {
+    if(!get_option('elb_widgetID')) echo('<div class="error"><p><strong>'.sprintf(__('Your Email List Builder Plugin is disabled. Please go to the <a href="%s">plugin settings</a> to enter a valid widget key.  Find your widget key by logging in at www.socialintents.com and selecting your Widget General Settings.  New to socialintents.com?  <a href="http://www.socialintents.com">Sign up for a Free Trial!</a>' ), admin_url('options-general.php?page=email-list-builder-by-socialintents')).'</strong></p></div>');
+}
+
+function elb_plugin_actions($links, $file) {
+    static $this_plugin;
+    if(!$this_plugin) $this_plugin = plugin_basename(__FILE__);
+    if($file == $this_plugin && function_exists('admin_url')) {
+        $settings_link = '<a href="'.admin_url('options-general.php?page=email-list-builder-by-socialintents').'">'.__('Settings', $elb_domain).'</a>';
+        array_unshift($links, $settings_link);
+    }
+    return($links);
+}
+
+function elb_add_settings_page() {
+    function elb_settings_page() {
+        global $elb_domain ?>
+<div class="wrap">
+        <?php screen_icon() ?>
+    <h2><?php _e('Email List Builder by Social Intents', $elb_domain) ?></h2>
+    <div class="metabox-holder meta-box-sortables ui-sortable pointer">
+        <div class="postbox" style="float:left;width:30em;margin-right:10px">
+            <h3 class="hndle"><span><?php _e('Email List Builder Settings', $elb_domain) ?></span></h3> 
+            <div class="inside" style="padding: 0 10px">
+                <form id="saveSettings" method="post" action="options.php">
+                    <p style="text-align:center"><?php wp_nonce_field('update-options') ?>
+			<a href="http://www.socialintents.com/" title="Email and Social Widgets that help grow your business">
+			<?php echo '<img src="'.plugins_url( 'socialintents.png' , __FILE__ ).'" height="50" width="200" "/> ';?></a></p>
+
+                    <p><label for="elb_widgetID"><?php printf(__('Enter your Widget Key below to activate the plugin.  If you don\'t have your key but have already signed up, you can <a href=\'http://www.socialintents.com\' target=\'_blank\'>login here</a> to grab your key under your widget --> your code snippet..<br>', $elb_domain), '<strong><a href="http://www.socialintents.com/" title="', '">', '</a></strong>') ?></label><br />
+			<input type="text" name="elb_widgetID" id="elb_widgetID" placeholder="Your Widget Key" value="<?php echo(get_option('elb_widgetID')) ?>" style="width:100%" />
+                    <p class="submit" style="padding:0"><input type="hidden" name="action" value="update" />
+                        <input type="hidden" name="page_options" value="elb_widgetID" />
+                        <input type="submit" name="elb_submit" id="elb_submit" value="<?php _e('Save Settings', $elb_domain) ?>" class="button-primary" /> 
+			</p>
+                 </form>
+            </div>
+        </div>
+        <div class="postbox" style="float:left;width:38em">
+            <h3 class="hndle"><span id="elb_noAccountSpan"><?php _e('No Account?  Sign up for a Free Social Intents Trial!', $elb_domain) ?></span></h3>
+            <div id="elb_register" class="inside" style="padding: -30px 10px">			
+		<p><?php printf(__('Social Intents is an email and social widgets platform that helps you grow your business with simple, effective plugins
+			with targeted rules and dynamic reports.
+			Please visit %1$sSocial Intents%2$ssocialintents.com%3$s to 
+				learn more.', $elb_domain), '<a href="
+http://www.socialintents.com/" title="', '">', '</a>') ?></p>
+			<b>Sign Up For a Free Trial Now!</b> (or register directly on our site at <a href="http://www.socialintents.com" target="_blank">Social Intents</a>)<br>
+			<input type="text" name="elb_email" id="elb_email" value="<?php echo(get_option('admin_email')) ?>" placeholder="Your Email" style="width:50%;margin:3px;" />
+			<input type="text" name="elb_name" id="elb_name" value="<?php echo(get_option('user_nicename')) ?>" placeholder="Your Name" style="width:50%;margin:3px;" />
+			<input type="password" name="elb_password" id="elb_password" value="" placeholder="Your Password" style="width:50%;margin:3px;" />
+			<br><input type="button" name="elb_inputRegister" id="elb_inputRegister" value="Register" class="button-primary" style="margin:3px;" /> 
+			
+			
+               
+            </div>
+	    <div id="elb_registerComplete" class="inside" style="padding: -20px 10px;display:none;">
+		<p>View reports, configure advanced settings, and export email subscribers on our website at <a href='http://www.socialintents.com'>www.socialintents.com</a>
+		</p><form id='saveDetailSettings' method="post" action="options.php">
+		<?php wp_nonce_field('update-options') ?>
+		<input type="hidden" name="action" value="update" />
+                <input type="hidden" name="page_options" value="elb_tab_text,elb_tab_placement,elb_header_text,elb_detail_text,elb_time_on_page,elb_tab_color" />
+		<table width="100%" >
+		<tr><td width="25%">Tab Text: </td>
+		<td >
+		<?php
+		if(get_option('elb_tab_text') ) {
+     		?>
+     			<input type="text" name="elb_tab_text" id="elb_tab_text" value="<?php echo(get_option('elb_tab_text')) ?>" style="margin:3px;width:100%;" />
+		
+    		<?php 
+			} else {
+   		?>
+			<input type="text" name="elb_tab_text" id="elb_tab_text" value="Subscribe Now!" style="margin:3px;width:100%;" />
+		<?php 
+			}
+   		?>
+		</td>
+		</tr>
+		<tr><td width="25%">Tab Color: </td>
+		<td >
+		<?php
+		if(get_option('elb_tab_color') && get_option('elb_tab_color') != '') {
+     		?>
+     			<input type="text" name="elb_tab_color" id="elb_tab_color" value="<?php echo(get_option('elb_tab_color')) ?>" style="margin:3px;width:100%;" />
+		
+    		<?php 
+			} else {
+   		?>
+			<input type="text" name="elb_tab_color" id="elb_tab_color" value="#00AEEF" style="margin:3px;width:100%;" />
+		<?php 
+			}
+   		?>
+		</td>
+		</tr>
+		<tr><td>Tab Placement: </td><td>
+		<?php 
+		if(get_option('elb_tab_placement') && get_option('elb_tab_placement') == 'bottom') {
+     		?>
+     		<select id="elb_tab_placement" name="elb_tab_placement">
+			<option value="bottom" selected>Bottom</option>
+			<option value="top">Top</option>
+			<option value="hide">Hide</option>
+		</select> 	
+    		<?php 
+			} else if(get_option('elb_tab_placement') == 'top') {
+   		?>
+		<select id="elb_tab_placement" name="elb_tab_placement">
+			<option value="bottom">Bottom</option>
+			<option value="top" selected>Top</option>
+			<option value="hide">Hide</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_tab_placement') == 'hide') {
+   		?>
+		<select id="elb_tab_placement" name="elb_tab_placement">
+			<option value="bottom">Bottom</option>
+			<option value="top">Top</option>
+			<option value="hide"  selected>Hide</option>
+		</select> 
+		<?php 
+			} else {
+   		?>
+		<select id="elb_tab_placement" name="elb_tab_placement">
+			<option value="bottom">Bottom</option>
+			<option value="top">Top</option>
+			<option value="hide">Hide</option>
+		</select> 
+		<?php 
+			}
+   		?>
+		
+		</td></tr>
+		<tr><td>When To Show Popup: </td><td>
+		<?php 
+		if(get_option('elb_time_on_page') && get_option('elb_time_on_page') == '0') {
+     		?>
+     		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0" selected>Immediately</option>
+			<option value="-1" >Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 	
+    		<?php 
+			} else if(get_option('elb_time_on_page') == '-1') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1" selected>Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_time_on_page') == '10') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10"  selected>10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select>  
+		<?php 
+			} else if(get_option('elb_time_on_page') == '15') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15"  selected>15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_time_on_page') == '20') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20"   selected>20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_time_on_page') == '30') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20" >20 Seconds</option>
+			<option value="30"  selected>30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_time_on_page') == '45') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20" >20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45"  selected>45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select> 
+		<?php 
+			} else if(get_option('elb_time_on_page') == '60') {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60"  selected>60 Seconds</option>
+		</select>  
+		<?php 
+			} else {
+   		?>
+		<select id="elb_time_on_page" name="elb_time_on_page">
+			<option value="0">Immediately</option>
+			<option value="-1">Ignore</option>
+			<option value="10">10 Seconds</option>
+			<option value="15">15 Seconds</option>
+			<option value="20">20 Seconds</option>
+			<option value="30">30 Seconds</option>
+			<option value="45">45 Seconds</option>
+			<option value="60">60 Seconds</option>
+		</select>  
+		<?php 
+			}
+   		?>
+		
+		</td></tr>
+		<tr><td>Header Text: </td><td>
+		<?php 
+		if(get_option('elb_header_text') && get_option('elb_header_text') != '') {
+     		?>
+     		<input type="text" name="elb_header_text" id="elb_header_text" value="<?php echo(get_option('elb_header_text')) ?>" style="margin:3px;width:100%;" />
+		
+    		<?php 
+			} else {
+   		?>
+		<input type="text" name="elb_header_text" id="elb_header_text" value="There you are - we\'ve been waiting for you!" style="margin:3px;width:100%;" />
+		<?php 
+			}
+   		?>
+		</td></tr>
+		<tr><td>Detail Text: </td>
+		<td>
+		<?php 
+		if(get_option('elb_detail_text') && get_option('elb_detail_text') != '') {
+     		?>
+     		<textarea rows="4" name="elb_detail_text" id="elb_detail_text" style="margin:3px;width:100%;"><?php echo(get_option('elb_detail_text')) ?></textarea>
+		<?php 
+			} else {
+   		?>
+		<textarea rows="4" name="elb_detail_text" id="elb_detail_text" style="margin:3px;width:100%;">Sign up now for the latest new, info, and updates!</textarea>
+		<?php 
+			}
+   		?>
+		</td></tr>
+		<tr><td></td><td>
+		<input id='elb_inputSaveSettings' type="button" value="<?php _e('Save Settings', $elb_domain) ?>" class="button-primary" /> 
+		<br><small >If you don't see your latest settings reflected in your site, please refresh your browser cache
+		or close and open the browser.
+		</small>	
+		</td></tr>
+		</table> 
+			
+		</form>
+	    </div>
+        </div>
+    </div>
+</div>
+<script>
+jQuery(document).ready(function($) {
+
+var elb_wid= $('#elb_widgetID').val();
+if (elb_wid=='') 
+{}
+else
+{
+	$( "#elb_register" ).hide();
+	$( "#elb_registerComplete" ).show();
+	$( "#elb_noAccountSpan" ).html("Configure you Email List Builder Widget");
+
+}
+$(document).on("click", "#elb_inputSaveSettings", function () {
+
+var elb_wid= $('#elb_widgetID').val();
+var elb_tt= $('#elb_tab_text').val();
+var elb_ht= $('#elb_header_text').val();
+var elb_dt= $('#elb_detail_text').val();
+var elb_tp= $('#elb_tab_placement').val();
+var elb_top= $('#elb_time_on_page').val();
+var url = 'https://www.socialintents.com/json/jsonSaveEmailSettings.jsp?tt='+elb_tt+'&ht='+elb_ht+'&wid='+elb_wid+'&dt='+elb_dt+'&tp='+elb_tp+'&top='+elb_top+'&callback=?';sessionStorage.removeItem("settings");
+$.ajax({
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallBack',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+       $('#elb_widgetID').val(json.key);
+	sessionStorage.removeItem("settings");
+	sessionStorage.setItem("hasSeenPopup","false");
+	$( "#saveDetailSettings" ).submit();
+	
+    },
+    error: function(e) {
+    }
+});
+
+  });
+
+$(document).on("click", "#elb_inputRegister", function () {
+
+var elb_email= $('#elb_email').val();
+var elb_name= $('#elb_name').val();
+var elb_password= $('#elb_password').val();
+var url = 'https://www.socialintents.com/json/jsonSignup.jsp?name='+elb_name+'&email='+elb_email+'&pw='+elb_password+'&callback=?';
+$.ajax({
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallBack',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+	if (json.msg=='') {
+         	$('#elb_widgetID').val(json.key);
+		alert("Thanks for signing up!");
+		$( "#saveSettings" ).submit();
+		
+	}
+	else {
+		alert(json.msg);
+	}
+    },
+    error: function(e) {
+       
+    }
+});
+
+});
+});
+
+</script>
+    <?php }
+    add_submenu_page('options-general.php', __('Social Intents', $elb_domain), __('Social Intents', $elb_domain), 'manage_options', 'email-list-builder-by-socialintents', 'elb_settings_page');
+}?>
---
+++ readme.txt
@@ -0,0 +1,93 @@
+=== Email List Builder by Social Intents ===
+Contributors: SocialIntents
+Tags: contact, form, contact form, feedback, ajax, email, email form, page, social plugin, social sharing, social widget, wordpress, plugin, admin, widget, sidebar, admin, widget, aweber, mailchimp, MailChimp plugin, newsletter, newsletters, popover, popup, share, sharing, social networking, social plugin, subscribe, subscribers, subscription, tweet, twitter, autoresponder, email, e-mail, email marketing, mailing, mailing list, opt-in, optin, opt-in plugin, webform
+Requires at least: 3.6
+Tested up to: 3.8.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add a customizable and targeted email list signup widget to any page.  Integrates with MailChimp, Constant Contact, and CSV.  
+
+== Changelog ==
+
+= 1.0.0 =
+* Intitial Release of the Email List Builder Widget
+
+
+== Description ==
+Email List Builder by Social Intents lets you quickly add a targeted and customizable popup form toany page to request visitors to join your email list.  It can be configured to display on a particular page, visit or after a specified time on a page.  Add your CSS, your language, and your style to the form.  It's one of the fastest and most effective ways to build your email lists.
+
+To configure the email list plugin, you need a Social Intents account.  Already have an accout?  Great!  Otherwise, sign up for a free trial at <a href="http://www.socialintents.com">www.socialintents.com</a>.
+
+Social Intents offers a free 14 day premium trial.  After that, it's free for up to 30 email subscribers a month.
+
+**Getting Started with Email List Builder**
+
+Step 1:  Login to WordPress Admin and search for the plugin 'Email List Builder by Social Intents' and install it.
+ 
+Step 2:  Register for a Free Trial Account right from the plugin or at our website.
+
+Questions or suggests?  Contact us at [socialintents.com](http://www.socialintents.com/).  We'd love to hear from you.
+
+
+
+**Why choose Email List Builder**
+
+* Simple to setup
+
+* Target specific pages for website visitors
+
+* Simple email integration with MailChimp and Constant Contact
+
+* Export email subscribes to a .csv file
+
+* Quickly build your email list subscribers
+
+* Customize the widget look and feel with custom CSS, logos, and messages
+
+
+
+To learn more about Social Intents visit us at [socialintents.com](http://www.socialintents.com/).
+
+
+== Installation ==
+
+1.  If you don't have a Social Intents account, you can sign up right from the widget.
+
+2.  In the admin section of your WordPress site, click 'plugins' and then 'add new'
+
+3.  Search for 'Email List Builder by Social Intents'
+
+4.  Click 'Install Now'
+
+5.  After Installation, enter your widget key or Register for a new Account.
+
+
+== Screenshots ==
+
+1. Email List Builder Settings
+2. Example Email List Builder Popup
+2. Social Intents Dashboard
+3. Social Intents Overview
+
+
+
+== Frequently Asked Questions ==
+
+= Do I need a Social Intents account for my chat widget =
+
+Yes, you do.  You can sign up for a free trial at [socialintents.com](http://www.socialintents.com/).. 
+
+= Do I have to install software on my server to get this working? =
+
+No, Email List Builder is a service. Simply sign up for the service, grab your widget key, and save it in your plugin.
+
+= Can I use Email List Builder for Free? =
+
+Yes absolutely, all Social Intents accounts include an always free 1 option for up to 30 new email subscribers a month.
+
+
+
+
+
